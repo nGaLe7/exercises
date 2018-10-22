399013f528394cea9853023db4e03f03e1711ca2
StudentNick <ngale24779@gmail.com>
2018-10-22 11:30:51 +1000

    asdfas

--- Gale_455111948_ICT50615_Intergrate_Datebase_Assessment_01/index.php
+++ Gale_455111948_ICT50615_Intergrate_Datebase_Assessment_01/index.php
@@ -17,8 +17,10 @@ session_start();
   <header class="nav">header</header>
 
   <nav class="nav">
-    <div class="menuItem"><a href="view/pages/mainLibary">View Books</a></div>
     <div class="menuItem"><a href="#">Home</a></div>
+    <div class="menuItem"><a href="view/pages/mainLibary">View Books</a></div>
+    <div class="menuItem"><a href="view/pages/mainLibary">Add Books</a></div>
+    
     
     <div class="menuItem"><a href="view/pages/createUser.php">Admin only</a></div>    
     <div class="menuItem">placeholder</div>
